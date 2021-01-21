d7aaab2bb209536af405548295d2181ae6f1df37
Sayed Muhammad Talha <syedtalha657@gmail.com>
2021-01-22 00:40:52 +0500

    Added homepage containing links to snippets

---
+++ snippets-info.js
@@ -0,0 +1,22 @@
+let snippets = [
+	{
+		name: "Card - Image Card with text overlay",
+		index: "index.html"
+	},
+	{
+		name: "Footer - Nike-Style",
+		index: "footer.html"
+	},
+	{
+		name: "Form - Subscribe Form",
+		index: "index.html"
+	},
+	{
+		name: "General - Audio Player",
+		index: "index.html"
+	},
+	{
+		name: "General - Circle CountBar controlled by Mousewheel",
+		index: "index.html"
+	}
+]
